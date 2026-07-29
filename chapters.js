03cda4b4cbdb5833894d457597a654f136943896
bloodveildawn <scinister69@gmail.com>
2026-07-30 00:03:18 +0900

    Create chapters.js

---
+++ chapters.js
@@ -0,0 +1,21 @@
+export const chapters = [
+
+{
+number:1,
+title:"Beast of Hell",
+url:"chapter1.html"
+},
+
+{
+number:2,
+title:"The Journey Begins",
+url:"chapter2.html"
+},
+
+{
+number:3,
+title:"A New Enemy",
+url:"chapter3.html"
+}
+
+];
