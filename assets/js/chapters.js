80a7565df66308e4591a92ef6e78def685b37309
bloodveildawn <scinister69@gmail.com>
2026-07-30 09:51:58 +0900

    ngapus / lagi

--- assets/js/chapters.js
+++ assets/js/chapters.js
@@ -2,19 +2,19 @@ export const chapters = [
 
 {
 number:1,
-title:"Beast of Hell",
+title:"1st Dawn: Beast of Hell",
 url:"chapter1.html"
 },
 
 {
 number:2,
-title:"Beneath The Surface",
+title:"2nd Dawn: Beneath The Surface",
 url:"chapter2.html"
 },
 
 {
 number:3,
-title:"TBD",
+title:"3rd Dawn",
 url:"chapter3.html"
 }
 
