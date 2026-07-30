ff907c3662eba4a5f9437d15b4012dddeac3da00
bloodveildawn <scinister69@gmail.com>
2026-07-30 08:48:07 +0700

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -13,7 +13,7 @@ const option = document.createElement("option");
 option.value = chapter.url;
 
 option.textContent =
-`Chapter ${chapter.number} - ${chapter.title}`;
+{chapter.title};
 
 select.appendChild(option);
 
