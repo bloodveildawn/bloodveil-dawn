e7d5ed5fa30e90c6fc10911f302792b6c948232e
bloodveildawn <scinister69@gmail.com>
2026-08-01 22:14:11 +0900

    test header

---
+++ test/assets/js/config.js
@@ -0,0 +1,13 @@
+const LINKS={
+
+home:"index.html",
+
+chapter:"chapter.html",
+
+character:"character.html",
+
+world:"world.html",
+
+support:"https://patreon.com/bloodveil"
+
+};
---
+++ test/assets/js/header.js
@@ -0,0 +1,25 @@
+function initializeHeader(){
+
+document
+.getElementById("homeLink")
+.href=LINKS.home;
+
+document
+.getElementById("chapterLink")
+.href=LINKS.chapter;
+
+document
+.getElementById("characterLink")
+.href=LINKS.character;
+
+document
+.getElementById("worldLink")
+.href=LINKS.world;
+
+document
+.getElementById("supportLink")
+.href=LINKS.support;
+
+initializeActiveNav();
+
+}
